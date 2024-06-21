ce37e4b54f83abce2079f785440a716ff4c4fe95
Sridana Windya <swindya@gmail.com>
2024-06-21 12:05:29 +0700

    Add files via upload

---
+++ Resources/dynamsoft.webtwain.config.js
@@ -0,0 +1,29 @@
+﻿//
+// Dynamsoft JavaScript Library for Basic Initiation of Dynamic Web TWAIN
+// More info on DWT: http://www.dynamsoft.com/Products/WebTWAIN_Overview.aspx
+//
+// Copyright 2017, Dynamsoft Corporation 
+// Author: Dynamsoft Team
+// Version: 12.2
+//
+/// <reference path="dynamsoft.webtwain.initiate.js" />
+var Dynamsoft = Dynamsoft || { WebTwainEnv: {} };
+
+Dynamsoft.WebTwainEnv.AutoLoad = true;
+///
+Dynamsoft.WebTwainEnv.Containers = [{ContainerId:'dwtcontrolContainer', Width:270, Height:350}];
+///
+Dynamsoft.WebTwainEnv.ProductKey = 'DE74D6D8523093667FD1400814E971945D559EBE4AB687E9B7C7FAF36B95D0305DB1BCAE581546DE876A3375DE84C319F30FFA3CDEE23444F4E014FB9C9BBA6E16F42D10BE531073F785F4D8D517E26E9349394F8D12E2B0FD32D49647F67F2FEC24F90617C61DFB982B0B0F0407B05000A4D41293';
+///
+Dynamsoft.WebTwainEnv.Trial = false;
+///
+Dynamsoft.WebTwainEnv.ActiveXInstallWithCAB = false;
+///
+// Dynamsoft.WebTwainEnv.ResourcesPath = 'Resources';
+
+/// All callbacks are defined in the dynamsoft.webtwain.install.js file, you can customize them.
+
+// Dynamsoft.WebTwainEnv.RegisterEvent('OnWebTwainReady', function(){
+// 		// webtwain has been inited
+// });
+
